019bb70dc7fc22cc21044908e06c94711826fa47
Sai Kartik <f20200435@pilani.bits-pilani.ac.in>
2021-04-15 18:56:23 +0530

    Added Location grabbing functionality(only latitude and longitude)

--- app/src/main/java/com/example/authenticator/ProfileActivity.java
+++ app/src/main/java/com/example/authenticator/ProfileActivity.java
@@ -1,13 +1,13 @@
 package com.example.authenticator;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +16,16 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -32,46 +42,54 @@ import com.squareup.picasso.Picasso;
 
 
 public class ProfileActivity extends AppCompatActivity {
-    private Button logout;
+    private Button logout, getLocation;
     private FirebaseUser user;
     private DatabaseReference reference;
     private String userID;
     private ImageView profileImage;
-    private Button changeDp,changeProfile;
+    private Button changeDp, changeProfile;
+    private TextView latitude,longitude;
     private StorageReference storageReference;
     private ProgressBar progressBar;
+    private AlertDialog.Builder builder;
+    private AlertDialog alertDialog;
+    private String lat, lon;
+
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        logout= findViewById(R.id.logout);
-        profileImage= findViewById(R.id.profileimage);
-        changeDp= findViewById(R.id.changedp);
-        changeProfile= findViewById(R.id.editprofilebutton);
-        progressBar= findViewById(R.id.checkloading);
+        logout = findViewById(R.id.logout);
+        profileImage = findViewById(R.id.profileimage);
+        changeDp = findViewById(R.id.changedp);
+        changeProfile = findViewById(R.id.editprofilebutton);
+        progressBar = findViewById(R.id.checkloading);
+        getLocation = findViewById(R.id.location);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(ProfileActivity.this,MainActivity.class));
+                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
             }
         });
-        user=FirebaseAuth.getInstance().getCurrentUser();
-        reference= FirebaseDatabase.getInstance().getReference("Users");
-        storageReference= FirebaseStorage.getInstance().getReference();
-        userID=user.getUid();
-        TextView nameview= findViewById(R.id.nameview);
-        TextView emailview= findViewById(R.id.emailview);
-        TextView hobbiesview= findViewById(R.id.hobbiesview);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        storageReference = FirebaseStorage.getInstance().getReference();
+        userID = user.getUid();
+        TextView nameview = findViewById(R.id.nameview);
+        TextView emailview = findViewById(R.id.emailview);
+        TextView hobbiesview = findViewById(R.id.hobbiesview);
         reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                User userprofile=snapshot.getValue(User.class);
-                if(userprofile!=null){
-                    String fullName=userprofile.fullName;
-                    String email=userprofile.email;
-                    String hobbies=userprofile.hobbies;
+                User userprofile = snapshot.getValue(User.class);
+                if (userprofile != null) {
+                    String fullName = userprofile.fullName;
+                    String email = userprofile.email;
+                    String hobbies = userprofile.hobbies;
                     nameview.setText(fullName);
                     emailview.setText(email);
                     hobbiesview.setText(hobbies);
@@ -83,8 +101,18 @@ public class ProfileActivity extends AppCompatActivity {
                 Toast.makeText(ProfileActivity.this, "Something went wrong. Here, have an F", Toast.LENGTH_LONG).show();
             }
         });
+        getLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((ContextCompat.checkSelfPermission(ProfileActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+                    ActivityCompat.requestPermissions(ProfileActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_LOCATION_PERMISSION);
+                } else {
+                    openDialogAndGetLocation();
+                }
+            }
+        });
 
-        StorageReference profileRef=storageReference.child("users/"+userID+"/profile.jpg");
+        StorageReference profileRef = storageReference.child("users/" + userID + "/profile.jpg");
         profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
             public void onSuccess(Uri uri) {
@@ -95,21 +123,58 @@ public class ProfileActivity extends AppCompatActivity {
         changeDp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(i,1000);
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, 1000);
             }
         });
 
         changeProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(v.getContext(),EditProfile.class);
-                intent.putExtra("fullnametag",nameview.getText().toString());
-                intent.putExtra("email",emailview.getText().toString());
-                intent.putExtra("hobbies",hobbiesview.getText().toString());
+                Intent intent = new Intent(v.getContext(), EditProfile.class);
+                intent.putExtra("fullnametag", nameview.getText().toString());
+                intent.putExtra("email", emailview.getText().toString());
+                intent.putExtra("hobbies", hobbiesview.getText().toString());
                 startActivity(intent);
             }
         });
+    }
+
+    private void openDialogAndGetLocation() {
+        builder = new AlertDialog.Builder(this);
+        final View locationPopup = getLayoutInflater().inflate(R.layout.locationpopup, null);
+        latitude = locationPopup.findViewById(R.id.latitude);
+        longitude = locationPopup.findViewById(R.id.longitude);
+
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Location permission required", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        LocationServices.getFusedLocationProviderClient(ProfileActivity.this).requestLocationUpdates(locationRequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                LocationServices.getFusedLocationProviderClient(ProfileActivity.this).removeLocationUpdates(this);
+                if(locationResult!=null&&locationResult.getLocations().size()>0){
+                    int latestLocationIndex=locationResult.getLocations().size()-1;
+                    double latitude=locationResult.getLocations().get(latestLocationIndex).getLatitude();
+                    double longitude=locationResult.getLocations().get(latestLocationIndex).getLongitude();
+                    lat=Double.toString(latitude);
+                    lon=Double.toString(longitude);
+                }
+            }
+        }, Looper.getMainLooper());
+
+        latitude= locationPopup.findViewById(R.id.latitude);
+        longitude= locationPopup.findViewById(R.id.longitude);
+        latitude.setText("Latitude:"+lat);
+        longitude.setText("Longitude:"+lon);
+        builder.setView(locationPopup);
+        alertDialog=builder.create();
+        alertDialog.show();
     }
 
     @Override
@@ -147,4 +212,7 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
 }
